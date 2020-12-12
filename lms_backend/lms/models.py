9a3239a7d124f18d1f5281610e9984ac42ce6b8a
NaEbrahimpour <nm313.isf@gmail.com>
2020-12-12 14:36:09 +0330

    modify api

--- lms_backend/lms/models.py
+++ lms_backend/lms/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
 class Assignment(models.Model):
     user_id = models.TextField()
     course_id = models.TextField()
     file_id = models.TextField()
     description = models.TextField(default='')
-    start_date =models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     deadline = models.DateTimeField(auto_now=False, auto_now_add=False)
 
 
@@ -15,5 +16,6 @@ class Assignment_answer(models.Model):
     user_id = models.TextField()
     course_id = models.TextField()
     File_id = models.TextField()
+    homework_number_id = models.TextField()
     description = models.TextField(default='')
     date_of_upload = models.DateTimeField(auto_now=False, auto_now_add=False)
