b7359387bacedf29a96e356abd94af3103bab4ec
NaEbrahimpour <nm313.isf@gmail.com>
2020-12-12 14:37:00 +0330

    modify api

--- lms_backend/lms/urls.py
+++ lms_backend/lms/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import assignment_upload
+from .views import assignment_upload, assignments_homeworklist, assignment_answer_upload
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^assignment_upload/', assignment_upload.as_view()),
+    url(r'^assignment_list/', assignments_homeworklist.as_view()),
+    url(r'^assignment_answer_upload/', assignment_answer_upload.as_view()),
+
 ]
